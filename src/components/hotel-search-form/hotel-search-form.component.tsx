9f8f88fa19ea7c590857268dbc79ff1e11dcfa80
vnav92 <vortexflameout@gmail.com>
2022-09-07 22:52:49 +0200

    Add base number input

--- src/components/hotel-search-form/hotel-search-form.component.tsx
+++ src/components/hotel-search-form/hotel-search-form.component.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, FormControl, FormLabel } from '@chakra-ui/react';
 import classNames from 'classnames';
 
 import styles from './hotel-search-form.module.scss';
+import { NumberInput } from '../number-input';
 
 type HotelSearchFormProps = {
   className?: string;
 };
 
+const adultsInputId = 'adults-input';
+const childrenInputId = 'children-input';
+
 export const HotelSearchForm: React.FC<HotelSearchFormProps> = ({
   className,
-}) => {
-  return <Box className={classNames(styles.formWrapper, className)}></Box>;
-};
+}) => (
+  <Box className={classNames(styles.formWrapper, className)}>
+    <FormControl className={styles.numberInputControl}>
+      <FormLabel htmlFor={adultsInputId} className={styles.numberInputLabel}>
+        Adults
+      </FormLabel>
+      <NumberInput id={adultsInputId} onValueChange={() => {}} />
+    </FormControl>
+    <FormControl className={styles.numberInputControl}>
+      <FormLabel htmlFor={childrenInputId} className={styles.numberInputLabel}>
+        Children
+      </FormLabel>
+      <NumberInput id={childrenInputId} onValueChange={() => {}} />
+    </FormControl>
+  </Box>
+);
---
+++ src/components/number-input/number-input.component.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Box,
+  NumberInput as ChakraNumberInput,
+  NumberInputField as ChakraNumberInputField,
+  Button,
+} from '@chakra-ui/react';
+import { AddIcon, MinusIcon } from '@chakra-ui/icons';
+
+import styles from './number-input.module.scss';
+
+type NumberInputProps = {
+  id: string;
+  onValueChange: (value: number) => void;
+};
+
+export const NumberInput: React.FC<NumberInputProps> = ({
+  id,
+  onValueChange,
+}) => {
+  const [currentValue, setCurrentValue] = useState(0);
+
+  useEffect(() => {
+    onValueChange(currentValue);
+  }, [currentValue, onValueChange]);
+
+  return (
+    <Box className={styles.numberInputWrapper}>
+      <Button
+        isDisabled={currentValue === 0}
+        onClick={() => setCurrentValue((value) => value - 1)}
+        className={styles.valueChangeButton}
+        aria-label="Subtract one from value"
+        background="transparent"
+      >
+        <MinusIcon />
+      </Button>
+      <ChakraNumberInput isReadOnly={true} value={currentValue}>
+        <ChakraNumberInputField id={id} className={styles.numberInputField} />
+      </ChakraNumberInput>
+      <Button
+        onClick={() => setCurrentValue((value) => value + 1)}
+        className={styles.valueChangeButton}
+        aria-label="Add one to value"
+        background="transparent"
+      >
+        <AddIcon />
+      </Button>
+    </Box>
+  );
+};
